60316164a45fd0b7bd65bd2ff61249be2911c282
Győri Sándor <alexaegis@gmail.com>
2018-09-18 21:40:15 +0200

    delete left

--- lesson02/task3_.py
+++ lesson02/task3_.py
@@ -23,10 +23,38 @@ class Simulation:
 def possibleCircuits(circuits, pointA, pointB):
     result = []
     for circuit in circuits:
-        if (circuit[0] == pointA and circuit[len(circuits) - 1] == pointB) or (circuit[0] == pointB and circuit[len(circuits) - 1] == pointA):
+        if (circuit[0] == pointA and circuit[len(circuit) - 1] == pointB) or (circuit[0] == pointB and circuit[len(circuit) - 1] == pointA):
             result.append(circuit)
 
     return result
+
+
+def allocateRoute(route, edges, demand, test=False):
+    valid = True
+    for index, val in enumerate(route):
+        if not index == 0:
+            edge = edges[route[index - 1], val]
+
+            # is there enough bandwith?
+            available_bandwith = edge["capacity"] - i
+
+            if hasattr(edge, "demand"):
+                available_bandwith = available_bandwith - \
+                    edge["demand"]["demand"]
+
+            if available_bandwith - demand["demand"] < 0:
+                # invalid no bandwith left
+                print "\tINVALID DEMAND, NO BANDWITH LEFT ON EDGE"
+                valid = False
+            else:
+                if not test:
+                    edge["demand"] = demand
+                    print "\t\tedges along the route appended with the demand: from: " + \
+                        str(route[index - 1]) + " to: " + \
+                        str(val) + " edge: " + str(edge)
+
+    print route
+    return valid
 
 
 g = nx.Graph()
@@ -45,24 +73,42 @@ with open(".\\lesson02\\cs1.json", "r") as file:
                    [1], capacity=link["capacity"])
 
     # simulation
-    currentDemands = []
     for i in range(data["simulation"]["duration"]):
-        print (i + 1)
+        print "iteration: " + str(i + 1)
 
         # is there any new request?
         for demand in data["simulation"]["demands"]:
             if demand["start-time"] == i + 1:
+                print "\tthere is a demand: " + str(demand)
                 # is it a valid request?
-                currentDemands.append(demand)
+                routes = possibleCircuits(
+                    data["possible-circuits"], demand["end-points"][0], demand["end-points"][1])
+                print "\tpossible routes for this demand: " + str(routes)
+
+                if len(routes) > 0:
+
+                    found = False
+                    gen = (route for route in routes if not found)
+                    print gen
+                    for route in gen:
+                        if allocateRoute(route, g.edges, demand, True):
+                            allocateRoute(route, g.edges, demand)
+                            print "\troute allocated"
+                            found = True
+                        else:
+                            print "\t couldn't allocated route"
+
                 print demand
 
-        # is there any requests to close?
-        for demand in currentDemands:
+            # is there any requests to close?
             if demand["end-time"] == i + 1:
-                currentDemands.remove(demand)
-                print demand
+                for edge in g.edges:
 
-    # this is for only drawing
+                    if hasattr(edge, "demand") and edge["demand"]["end-points"] == demand["end-points"]:
+                        print "Hooray"
+                        delattr(edge, "demand")
+
+            # this is for only drawing
     pos = nx.spring_layout(g)  # positions for all nodes
     edges = [(u, v) for (u, v, d) in g.edges(data=True)]
     # nodes
