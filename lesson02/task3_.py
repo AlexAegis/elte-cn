ba7387d031b148590c0d7b27b00c1c65d0f46dca
Győri Sándor <alexaegis@gmail.com>
2018-09-18 22:31:12 +0200

    added animation

--- lesson02/task3_.py
+++ lesson02/task3_.py
@@ -2,6 +2,62 @@ import this
 import json
 import networkx as nx
 import matplotlib.pyplot as plt
+import matplotlib.animation
+import time
+
+# Build plot
+fig, ax = plt.subplots(figsize=(6, 4))
+g = nx.Graph()
+
+
+def iteration(i):
+    print "UPDATE!!!!!!!!!!!"
+    ax.clear()
+    print "iteration: " + str(i + 1)
+
+    # is there any new request?
+    for demand in data["simulation"]["demands"]:
+        if demand["start-time"] == i + 1:
+            print "\tthere is a demand: " + str(demand)
+            # is it a valid request?
+            routes = possibleCircuits(
+                data["possible-circuits"], demand["end-points"][0], demand["end-points"][1])
+            print "\tpossible routes for this demand: " + str(routes)
+
+            if len(routes) > 0:
+
+                found = False
+                gen = (route for route in routes if not found)
+                for route in gen:
+                    if allocateRoute(route, g.edges, demand, True):
+                        allocateRoute(route, g.edges, demand)
+                        print "\t\troute allocated"
+                        found = True
+                    else:
+                        print "\t\tcouldn't allocated route"
+
+        # is there any requests to close?
+        if demand["end-time"] == i + 1:
+            for edge in g.edges:
+                if "demand" in g.edges[edge]:
+                    if g.edges[edge]["demand"] == demand:
+                        print "\tdelete demand between: " + str(edge) + " demand: " +\
+                            str(g.edges[edge]["demand"])
+                        del g.edges[edge]["demand"]
+
+    # this is for only drawing
+
+    edges = [(u, v) for (u, v, d) in g.edges(data=True)]
+    # nodes
+    nx.draw_networkx_nodes(g, pos, node_size=700)
+
+    # edges
+    nx.draw_networkx_edges(g, pos, edgelist=edges,
+                           width=6)
+    # labels
+    nx.draw_networkx_labels(g, pos, font_size=20, font_family='sans-serif')
+    nx.draw_networkx_edge_labels(
+        g, pos, font_size=12, font_family='sans-serif')
 
 
 def possibleCircuits(circuits, pointA, pointB):
@@ -39,8 +95,6 @@ def allocateRoute(route, edges, demand, test=False):
     return valid
 
 
-g = nx.Graph()
-
 with open(".\\lesson02\\cs1.json", "r") as file:
     data = json.load(file)
 
@@ -54,53 +108,11 @@ with open(".\\lesson02\\cs1.json", "r") as file:
         g.add_edge(link["points"][0], link["points"]
                    [1], capacity=link["capacity"])
 
-    # simulation
-    for i in range(data["simulation"]["duration"]):
-        print "iteration: " + str(i + 1)
-
-        # is there any new request?
-        for demand in data["simulation"]["demands"]:
-            if demand["start-time"] == i + 1:
-                print "\tthere is a demand: " + str(demand)
-                # is it a valid request?
-                routes = possibleCircuits(
-                    data["possible-circuits"], demand["end-points"][0], demand["end-points"][1])
-                print "\tpossible routes for this demand: " + str(routes)
-
-                if len(routes) > 0:
-
-                    found = False
-                    gen = (route for route in routes if not found)
-                    for route in gen:
-                        if allocateRoute(route, g.edges, demand, True):
-                            allocateRoute(route, g.edges, demand)
-                            print "\t\troute allocated"
-                            found = True
-                        else:
-                            print "\t\tcouldn't allocated route"
-
-            # is there any requests to close?
-            if demand["end-time"] == i + 1:
-                for edge in g.edges:
-                    if "demand" in g.edges[edge]:
-                        if g.edges[edge]["demand"] == demand:
-                            print "\tdelete demand between: " + str(edge) + " demand: " +\
-                                str(g.edges[edge]["demand"])
-                            del g.edges[edge]["demand"]
-
-            # this is for only drawing
     pos = nx.spring_layout(g)  # positions for all nodes
-    edges = [(u, v) for (u, v, d) in g.edges(data=True)]
-    # nodes
-    nx.draw_networkx_nodes(g, pos, node_size=700)
+    # simulation
+    # for i in range(data["simulation"]["duration"]):
+    # iteration(i)
 
-    # edges
-    nx.draw_networkx_edges(g, pos, edgelist=edges,
-                           width=6)
-    # labels
-    nx.draw_networkx_labels(g, pos, font_size=20, font_family='sans-serif')
-    nx.draw_networkx_edge_labels(
-        g, pos, font_size=12, font_family='sans-serif')
-
-    plt.axis('off')
+    ani = matplotlib.animation.FuncAnimation(
+        fig, iteration, frames=11, interval=500, repeat=True)
     plt.show()
