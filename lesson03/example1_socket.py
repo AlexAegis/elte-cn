f98019d12f8eed252cb1a0c48127ee570ac9d036
Győri Sándor <alexaegis@gmail.com>
2018-09-30 19:25:41 +0200

    added lesson 3 task 1

---
+++ lesson03/example1_socket.py
@@ -0,0 +1,13 @@
+""" raw http socket query
+"""
+import socket
+
+for response in socket.getaddrinfo('www.python.org', 'http'):
+	print response
+
+for response in socket.getaddrinfo('www.python.org', 'http', socket.AF_INET,
+                                   socket.SOCK_STREAM, socket.IPPROTO_TCP,
+                                   socket.AI_CANONNAME):
+	print 'lol'
+	family, socktype, proto, cannonname, sockaddr = response
+	print family, socktype, proto, cannonname, sockaddr
--- lesson03/task1_socket.py
+++ lesson03/task1_socket.py
@@ -1,18 +1,41 @@
-""" Example 1
-Sockets
+""" Task 1
+a) Print out the result of the socket.gethostname() function
+b) Call the gethostbyname() and the gethostbyname_ex()
+functions for these addresses:
+- 'homer', 'www.python.org', 'inf.elte.hu'
+c) Using the gethostbyaddr() function query the hostname
+of the following IP addresses:
+- '157.181.161.79', '185.43.207.92'
 """
 
 import socket
 
-print socket.gethostname()
-print socket.gethostbyname('www.python.org')
+# a)
+print "socket.gethostname(): " + socket.gethostname()
 
-for response in socket.getaddrinfo('www.python.org', 'http'):
-	print response
+# b)
+try:
+	print "socket.gethostbyname('homer'): " + socket.gethostbyname('homer')
+except socket.gaierror as error:
+	print "socket.gethostbyname('homer') is not accessible: " + str(error)
 
-for response in socket.getaddrinfo('www.python.org', 'http', socket.AF_INET,
-                                   socket.SOCK_STREAM, socket.IPPROTO_TCP,
-                                   socket.AI_CANONNAME):
-	print 'lol'
-	family, socktype, proto, cannonname, sockaddr = response
-	print family, socktype, proto, cannonname, sockaddr
+print "socket.gethostbyname('www.python.org'): " + socket.gethostbyname(
+    'www.python.org')
+print "socket.gethostbyname('inf.elte.hu'): " + socket.gethostbyname(
+    'inf.elte.hu')
+try:
+	print "socket.gethostbyname_ex('homer'): " + str(
+	    socket.gethostbyname_ex('homer'))
+except socket.gaierror as error:
+	print "socket.gethostbyname_ex('homer') is not accessible: " + str(error)
+
+print "socket.gethostbyname_ex('www.python.org'): " + str(
+    socket.gethostbyname_ex('www.python.org'))
+print "socket.gethostbyname_ex('inf.elte.hu'): " + str(
+    socket.gethostbyname_ex('inf.elte.hu'))
+
+# c)
+print "socket.gethostbyaddr('157.181.161.79'): " + str(
+    socket.gethostbyaddr('157.181.161.79'))
+print "socket.gethostbyaddr('185.43.207.92'): " + str(
+    socket.gethostbyaddr('185.43.207.92'))
