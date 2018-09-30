a33533e1a5b3da1464caf0a7e6953ff479ca973c
Győri Sándor <alexaegis@gmail.com>
2018-09-30 19:29:44 +0200

    second task added

--- lesson03/task2_http.py
+++ lesson03/task2_http.py
@@ -1,11 +1,16 @@
-""" raw http socket query
+""" Task 2
+
+a) Query the 'http' informations of 'www.python.org'
+
+b) Query the 'http' servers name of 'www.inf.elte.hu'
+with the name flag 'AI_CANNONNAME'
 """
 import socket
-
+# a)
 for response in socket.getaddrinfo('www.python.org', 'http'):
 	print response
-
-for response in socket.getaddrinfo('www.python.org', 'http', socket.AF_INET,
+# b)
+for response in socket.getaddrinfo('www.inf.elte.hu', 'http', socket.AF_INET,
                                    socket.SOCK_STREAM, socket.IPPROTO_TCP,
                                    socket.AI_CANONNAME):
 	family, socktype, proto, cannonname, sockaddr = response
